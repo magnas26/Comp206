bf5a775a68db50c2321e361abaf1e20d849fb9ec
Sangam CHOUCHAN <schouc2@teaching.CS.McGill.CA>
2015-03-22 17:56:12 -0400

    project now ready to be coded

--- list.c
+++ list.c
@@ -1,18 +1,38 @@
 #include "list.h"
 
-void add (int a) {
+struct NODE{
+int number; 
+struct NODE* next;
+
+}aNode;  
+
+//adding an element to a linkedlist
+void add (int aNum) {
 
 
 }
 
+//print every concent of the linkedlist
 void prettyPrint(){
 
 
 }
 
+//search through linkedlist and return pointer that points to the list struct containing the matched number
+struct NODE *find(int aNum){
+struct NODE *ptr; 
+struct NODE a; 
+ptr=&a;   
+return ptr;  
+
+}
+
+
+//call find function, if a node is found return true, else false
 BOOLEAN delete (int a){
 
 return 0; 
 }
+
 
 
