3809502ead68c6f3bd1d806f8c90d56f6fb94168
Sangam CHOUCHAN <schouc2@teaching.CS.McGill.CA>
2015-03-22 19:11:25 -0400

    asking user to input filename finished

--- main.c
+++ main.c
@@ -1,8 +1,20 @@
 #include "list.h" 
-
+#include <stdio.h>
 int main(){
+//ask user to repeatedly input file name or path until valid 
+char fileName [100]; 
+printf("Pleaser enter a file name: \n"); 
+scanf("%s", fileName);
+FILE *ptr; 
+ptr = fopen(fileName, "r");
+while(ptr == NULL ) {
+printf("File not found...please enter again:  \n");
+scanf("%s", fileName); 
+ptr = fopen(fileName, "r");
+}
+//read each number and put it in the LinkedList 
 
-prettyPrint(); 
 
 
 }
+
